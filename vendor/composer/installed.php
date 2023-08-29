e28fb673f846b8c6683cee1d375a39c1cd80686e
Minit-WangMaixin <3073486251@qq.com>
2023-08-29 17:41:56 +0800

    Class

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'administrator/ningning',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ac253066e8703d74d6476f6fc1a84a3dec1700c7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'administrator/ningning' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ac253066e8703d74d6476f6fc1a84a3dec1700c7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
